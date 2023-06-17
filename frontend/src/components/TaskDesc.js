b1ba019e25665810c02a83ada6f55d5a4f0dfec4
Atharva Tyagi <126762501+ty4g1@users.noreply.github.com>
2023-06-18 00:01:46 +0400

    Updated priority classification

--- frontend/src/components/TaskDesc.js
+++ frontend/src/components/TaskDesc.js
@@ -80,7 +80,7 @@ const TaskDesc = ({task}) => {
                 <p>from <b style={{color: "white"}}>{format(new Date(task.startTime), "hh:mm a")}</b> on <b style={{color: "white"}}>{format(new Date(task.startTime), "do MMM Y")}</b></p>
                 <p>to <b style={{color: "white"}}>{format(new Date(task.endTime), "hh:mm a")}</b> on <b style={{color: "white"}}>{format(new Date(task.endTime), "do MMM Y")}</b></p>
                 <p style={{margin: '10px 0px'}}>Tags: {task.tags.map(tag => <b key={tag} className="tag" style={{color: 'white', border: '1px white solid', margin: '5px', padding: '5px', borderRadius: '20px'}}>{tag} </b>)}</p>
-                <p>Priority Score: <b style={{color: "white"}}>{task.priority} ({task.priority < 25 ? 'Low' : task.priority < 75 ? 'Medium' : 'High'})</b></p>
+                <p>Priority Score: <b style={{color: "white"}}>{task.priority} ({task.priority < 34 ? 'Low' : task.priority < 67 ? 'Medium' : 'High'})</b></p>
                 <span className="material-symbols-outlined delete" onClick={handleClickDel}>delete</span>
                 {!task.completed && 
                 <div>
