c67b73b1344d4fd65a457aab6a1ab1a390290b79
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2023-07-03 17:12:14 +0400

    Updated sendEmail.js

--- backend/services/sendEmail.js
+++ backend/services/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+//setup transporter
 let transporter = nodemailer.createTransport({
     host: 'smtp.zoho.eu',
     port: 465,
@@ -11,6 +12,7 @@ let transporter = nodemailer.createTransport({
     }
 });
 
+//verfiy connection configuration
 transporter.verify((error, success) => {
     if (error) {
         console.log(error);
@@ -20,4 +22,20 @@ transporter.verify((error, success) => {
 }
 );
 
+const sendEmail = (email, subject, text) => {
+    let mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: subject,
+        text: text
+    };
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent');
+        }
+    });
+}
 
+module.exports = sendEmail;
