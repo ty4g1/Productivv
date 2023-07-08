3af1d2c487f5dcb8e7910be8bc103548bdb26a86
Jim Seah <134794564+jimseah-0116@users.noreply.github.com>
2023-07-08 13:21:00 +0800

    Changing to Webhook
    Removed bot.launch() method and added exports.handler: Switching from polling to webhook

--- bot/src/index.js
+++ bot/src/index.js
@@ -98,4 +98,13 @@ bot.action('date', async (ctx) => {
     });
 });
 
-bot.launch();
+// bot.launch(); - No longer using polling, using webhook instead.
+
+exports.handler = (event, context, callback) => {
+    const tmp = JSON.parse(event.body); // get data passed to us
+    bot.handleUpdate(tmp); // make Telegraf process that data
+    return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
+        statusCode: 200,
+        body: '',
+    });
+};
