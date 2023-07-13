d71a717020cfb617c2af603a788e85abd371a4f3
Jim Seah <134794564+jimseah-0116@users.noreply.github.com>
2023-07-14 00:45:23 +0800

    Comments and help request

--- bot/src/index.js
+++ bot/src/index.js
@@ -5,6 +5,7 @@ const { Telegraf } = require('telegraf');
 const { message } = require('telegraf/filters');
 const { _ } = require('underscore');
 
+// Functions for fetching User and Tasks from Productivv
 const fetchUser = async (username) => {
     const response = await fetch('http://localhost:4000/api/user/find/' + username, {
         headers: {
@@ -19,7 +20,6 @@ const fetchUser = async (username) => {
     if (response.ok) {
         return user;
     }
-
 }
 
 const fetchTasks = async (token) => {
@@ -39,8 +39,9 @@ const fetchTasks = async (token) => {
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
-console.log('Bot is starting...');
+console.log('Bot is running');
 
+// Inputs user's Telegram username into fetchUser function - fetches user token and stores it in 'ctx' object
 bot.use(async (ctx, next) => {
     ctx.state.user = await fetchUser(ctx.from.username);
     next(ctx);
@@ -48,37 +49,43 @@ bot.use(async (ctx, next) => {
 
 // Basic commands
 bot.start((ctx) => {
-    ctx.reply('Hi! To get started, enter your Telegram username in the Productivv app, then type /tasks.');
+    ctx.reply('Hi! To get started, enter your Telegram username in the Productivv app, then use /help for more info.');
 })
 
 bot.help((ctx) => {
-    ctx.reply('Please visit https://productivv.netlify.app/help for help!');
+    ctx.reply('To view your tasks, use /tasks. To set-up reminders, use /reminders.');
 })
 
 bot.on(message('sticker'),
-    (ctx) => ctx.reply("That's a cool sticker!"));
+    (ctx) => ctx.reply("That's a cool sticker!")); // Simple response to sticker messages
+    
+// Tasks command to easily view tasks through Telegram
+/* NOTE: Need help here, priority and deadline buttons don't work properly. sortedTasks array seems to be correct
+   but the bot does not display tasks in the correct order. */
 
 bot.command('tasks', async (ctx) => {
-    bot.telegram.sendMessage(ctx.chat.id, 'Would you like to view your tasks by priority or date?',
+    bot.telegram.sendMessage(ctx.chat.id, 'Would you like to view your tasks by priority or deadline?',
        { reply_markup: {
             inline_keyboard: [
                 [
                     { text: 'Priority', callback_data: 'prio' },
-                    { text: 'Date', callback_data: 'date' }
+                    { text: 'Deadline', callback_data: 'deadline' }
                 ]
             ]
         }
     })
 });
 
-bot.action('prio', async (ctx) => {
+bot.action('prio', async (ctx) => { // View tasks by priority
     ctx.answerCbQuery();
     ctx.reply('These are your current tasks by priority:');
+
     const tasks = await fetchTasks(ctx.state.user.token);
-    // console.log('first', tasks);
-    // const sortedTasks = _.sortBy(tasks, 'priority');
-    // console.log('then', sortedTasks);
-    tasks.forEach(task => {
+        console.log('Fetched Array', tasks); // FOR DEBUGGING PURPOSES
+    const sortedTasks = _.sortBy(tasks, 'priority');
+        console.log('Sorted Array', sortedTasks); // FOR DEBUGGING PURPOSES
+
+    sortedTasks.forEach(task => {
         ctx.reply(`${task.title} from *${format(new Date(task.startTime), "hh:mm a")}* on *${format(new Date(task.startTime), "do MMM Y")}* to *${format(new Date(task.endTime), "hh:mm a")}* on *${format(new Date(task.endTime), "do MMM Y")}*, with tags:${task.tags.map(tag => ` *${tag}* `)}`,
         {
             parse_mode: 'Markdown'
@@ -86,11 +93,13 @@ bot.action('prio', async (ctx) => {
     });
 });
 
-bot.action('date', async (ctx) => {
+bot.action('deadline', async (ctx) => { // View tasks by ending date & time
     ctx.answerCbQuery();
-    ctx.reply('These are your current tasks by date:');
+    ctx.reply('These are your current tasks by deadline:');
     const tasks = await fetchTasks(ctx.state.user.token);
-    tasks.forEach(task => {
+    const sortedTasks = _.sortBy(tasks, 'endTime');
+
+    sortedTasks.forEach(task => {
         ctx.reply(`${task.title} from *${format(new Date(task.startTime), "hh:mm a")}* on *${format(new Date(task.startTime), "do MMM Y")}* to *${format(new Date(task.endTime), "hh:mm a")}* on *${format(new Date(task.endTime), "do MMM Y")}*, with tags:${task.tags.map(tag => ` *${tag}* `)}`,
         {
             parse_mode: 'Markdown'
@@ -98,13 +107,5 @@ bot.action('date', async (ctx) => {
     });
 });
 
-// bot.launch(); - No longer using polling, using webhook instead.
 
-exports.handler = (event, context, callback) => {
-    const tmp = JSON.parse(event.body); // get data passed to us
-    bot.handleUpdate(tmp); // make Telegraf process that data
-    return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
-        statusCode: 200,
-        body: '',
-    });
-};
+bot.launch();
