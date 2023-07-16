d82e3317345facdb6dcf2203be40bf8085e77cb6
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2023-07-16 18:10:30 +0400

    Added clear past due tasks functionality

--- frontend/src/pages/PastDue.js
+++ frontend/src/pages/PastDue.js
@@ -26,8 +26,27 @@ const PastDue = () => {
         
         
     }, [dispatch, user]);
+    const handleClear = async (e) => {
+        e.preventDefault();
+        if (!tasks.filter((task) => new Date(task.endTime) < new Date()).length) {
+            return;
+        }
+        if (!window.confirm('Are you sure you want to clear all past due tasks?')) {
+            return;
+        }
+        tasks.filter((task) => new Date(task.endTime) < new Date()).forEach(async (task) => {
+            await fetch('/api/tasks/' + task._id, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            });
+            dispatch({type: 'DELETE_TASK', payload: task});
+        });
+    }
     return ( 
         <div className="pastdue">
+            <button className='clear' onClick={handleClear}>Clear Past Due</button>
             <div className="tasks">
             {(!tasks.filter((task) => new Date(task.endTime) < new Date()) 
               || !tasks.filter((task) => new Date(task.endTime) < new Date()).length) && 
