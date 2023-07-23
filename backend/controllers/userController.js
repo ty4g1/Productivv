0855a49107c0a0d573ba3f99b565c4750d838947
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2023-07-23 22:33:23 +0400

    fix points bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -173,7 +173,7 @@ const updateUserPoints = async (req, res) => {
     const body = req.body;
     try {
         const user = await User.updatePoints(body);
-        user.points = body.points;
+        user.points += body.points;
         res.status(200).json(user);
     } catch (error) {
         res.status(400).json({error: error.message});
